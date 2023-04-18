c98e4501fdb87453726ff144a4a16bbc04dca306
AmbryN <nicolas.ambry@gmail.com>
2023-04-18 16:13:15 +0200

    Added user creation in UserController

--- src/main/java/dev/ambryn/discordtest/controllers/UserController.java
+++ src/main/java/dev/ambryn/discordtest/controllers/UserController.java
@@ -1,18 +1,24 @@
 package dev.ambryn.discordtest.controllers;
 
 import dev.ambryn.discordtest.beans.Role;
+import dev.ambryn.discordtest.beans.User;
 import dev.ambryn.discordtest.dto.RoleAddDTO;
+import dev.ambryn.discordtest.dto.UserCreateDTO;
 import dev.ambryn.discordtest.dto.UserGetDTO;
 import dev.ambryn.discordtest.dto.mappers.dto.UserMapper;
 import dev.ambryn.discordtest.enums.ERole;
+import dev.ambryn.discordtest.errors.DataAccessException;
 import dev.ambryn.discordtest.filters.Authorize;
 import dev.ambryn.discordtest.repositories.RoleRepository;
 import dev.ambryn.discordtest.repositories.UserRepository;
+import dev.ambryn.discordtest.responses.Created;
 import dev.ambryn.discordtest.responses.NotFound;
 import dev.ambryn.discordtest.responses.Ok;
+import dev.ambryn.discordtest.responses.ServerError;
 import dev.ambryn.discordtest.validators.BeanValidator;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,6 +58,23 @@ public class UserController {
                 .map(UserMapper::toFinestDto)
                 .map(Ok::build)
                 .orElseGet(() -> NotFound.build("Could not find user with id=" + id));
+    }
+
+    @POST
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @Authorize(level = ERole.ADMIN)
+    public Response saveUser(UserCreateDTO userDTO) {
+        logger.debug("Saving user={}", userDTO);
+        BeanValidator.validate(userDTO);
+
+        User user = UserMapper.toUser(userDTO);
+        try {
+            userRepository.saveUser(user);
+            return Created.build(UserMapper.toDto(user));
+        } catch (DataAccessException dae) {
+            return ServerError.build(dae.getMessage());
+        }
     }
 
     @POST
